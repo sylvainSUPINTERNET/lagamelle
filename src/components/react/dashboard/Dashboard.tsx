95cc67ad14240bcfe2e976fce0934784cd5c3051
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-04-17 00:40:33 +0200

    chore(card):card

--- src/components/react/dashboard/Dashboard.tsx
+++ src/components/react/dashboard/Dashboard.tsx
@@ -7,6 +7,7 @@ import { BiPaint } from "react-icons/bi";
 import { motion } from "framer-motion";
 import { FaPhotoVideo } from "react-icons/fa";
 import { FaExclamationCircle } from "react-icons/fa";
+import { GiChiliPepper } from "react-icons/gi";
 
 import riz_curry from '../../../assets/riz_curry.png'
 
@@ -20,31 +21,43 @@ export default function Dashboard({userDetail}: Record<string, any>) {
 
     return ( 
         <div className="h-screen bg-white flow-root">
-            <div className="relative">
-                <svg
-                    className="z-0 absolute w-full h-[320px] md:h-[320px]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 1440 320"
-                    preserveAspectRatio="none">
-                        <path
-                        fill="#FFB84C"
-                        fillOpacity="1"
-                        d="M0,96L40,90.7C80,85,160,75,240,96C320,117,400,171,480,197.3C560,224,640,224,720,213.3C800,203,880,181,960,176C1040,171,1120,181,1200,181.3C1280,181,1360,171,1400,165.3L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
-                        ></path>
-                </svg>
-                <div className="absolute z-1 w-full h-full0">
-                    <motion.div
-                        initial={{ opacity: 0, y: 30 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.6, ease: "easeOut", delay: 0.2 }}>
-                        <div className="bg-red-600 container mx-auto w-3/4 md:w-full mt-4 p-2">
-                            content
-                        </div>
-                    </motion.div>
-
+            <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.2, ease: "easeOut", delay: 0.2 }}>
+                <div className="bg-red-600 container mx-auto w-3/4 md:w-full mt-4 p-2">
+                    content
                 </div>
-            </div>
+            </motion.div>
 
+            <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.4, ease: "easeOut", delay: 0.2 }}>
+                <div className="flex container mx-auto mt-[4em]">
+                    <div className="shadow-md bg-white relative">
+                        <div className="absolute p-[0.4em] top-[0] bg-emerald-600 rounded-lg shadow-sm font-medium text-white">
+                            492 Kcal
+                        </div>
+                        <img src={riz_curry.src} alt="Riz Curry" className="w-[170PX] h-auto rounded-t-lg object-cover" />
+                        <div className="text-center py-2 font-medium text-lg">
+                            <p className="py-1 mb-1">Riz Poulet Curry</p>
+                            <div className="flex justify-center items-center gap-2 mb-1 py-1">
+                                <GiChiliPepper className="text-red-600 text-2xl"/>
+                                <GiChiliPepper className="text-slate-600 text-2xl"/>
+                                <GiChiliPepper className="text-slate-600 text-2xl"/>
+                            </div>
+                            <div className="flex justify-center py-2">
+                                <div className="w-3/4 shadow-sm mb-1 rounded-full bg-[#FFB84C] py-1 px-4 transition-colors duration-200 hover:bg-[#FFA24B] cursor-pointer text-white font-medium hover:text-black transition-colors duration-200">
+                                    <button className="">
+                                        Add
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </motion.div>
 
         </div>
     )
