4d3d5b4734a4888eb8396485bf2bb5ca6f67761c
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-04-18 17:13:44 +0200

    chore(wao): nice

--- src/components/react/dashboard/Dashboard.tsx
+++ src/components/react/dashboard/Dashboard.tsx
@@ -14,6 +14,7 @@ import sauc from "../../../assets/sauc.png"
 import { HiPlus } from "react-icons/hi";
 import { CiShoppingBasket } from "react-icons/ci";
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { FaPlus } from "react-icons/fa";
 
 
 export default function Dashboard({userDetail}: Record<string, any>) {
@@ -35,46 +36,46 @@ export default function Dashboard({userDetail}: Record<string, any>) {
                         <div className="flex-1">
                             <p className="text-sm md:text-lg text-gray-700 font-medium">🔥 Promo 20% sur votre première commande avec le code <span className="bg-orange-500 text-white text-md md:text-2xl p-1 rounded-lg font-bold font-mono shadow-sm">LAGAM2025</span></p>
                         </div>
-                        <div className="ml-4">
+                        {/* <div className="ml-4">
 
                         <Menu>
-                            <MenuButton className="rounded-full p-3 transition shadow-sm border-2 border-slate-200 hover:border-slate-400">
-                                <CiShoppingBasket className="cursor-pointer text-6xl text-gray-800 hover:text-black transition" />
-                            </MenuButton>
+                        <MenuButton className="rounded-full p-3 transition shadow-sm border-2 border-slate-200 hover:border-slate-400">
+                            <CiShoppingBasket className="cursor-pointer text-6xl text-gray-800 hover:text-black transition" />
+                        </MenuButton>
 
-                            <MenuItems
-                            transition
-                            anchor="bottom end"
-                            className="bg-black w-52 origin-top-right rounded-xl border border-white/5 bg-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0">
-                                    <MenuItem>
-                                        <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                                        Edit
-                                        <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘E</kbd>
-                                        </button>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                                        Duplicate
-                                        <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd>
-                                        </button>
-                                    </MenuItem>
-                                    <div className="my-1 h-px bg-white/5" />
-                                    <MenuItem>
-                                        <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                                        Archive
-                                        <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘A</kbd>
-                                        </button>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
-                                        Delete
-                                        <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd>
-                                        </button>
-                                    </MenuItem>
-                                </MenuItems>
-                            </Menu>
+                        <MenuItems
+                        transition
+                        anchor="bottom end"
+                        className="bg-slate-900 w-52 origin-top-right rounded-xl border border-white/5 bg-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0">
+                                <MenuItem>
+                                    <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+                                    Edit
+                                    <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘E</kbd>
+                                    </button>
+                                </MenuItem>
+                                <MenuItem>
+                                    <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+                                    Duplicate
+                                    <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd>
+                                    </button>
+                                </MenuItem>
+                                <div className="my-1 h-px bg-white/5" />
+                                <MenuItem>
+                                    <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+                                    Archive
+                                    <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘A</kbd>
+                                    </button>
+                                </MenuItem>
+                                <MenuItem>
+                                    <button className="group flex w-full items-center gap-2 rounded-lg py-1.5 px-3 data-[focus]:bg-white/10">
+                                    Delete
+                                    <kbd className="ml-auto hidden font-sans text-xs text-white/50 group-data-[focus]:inline">⌘D</kbd>
+                                    </button>
+                                </MenuItem>
+                            </MenuItems>
+                        </Menu>
                             
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </motion.div>
@@ -87,7 +88,6 @@ export default function Dashboard({userDetail}: Record<string, any>) {
                 
 
                     <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-2">
-
                         {
                             [1,2,3,4,5,6].map( (item, index:number) => {
                                 return (
@@ -95,7 +95,8 @@ export default function Dashboard({userDetail}: Record<string, any>) {
                                         initial={{ opacity: 0, y: 30 }}
                                         animate={{ opacity: 1, y: 0 }}
                                         transition={{ duration: 0.4, ease: "easeOut", delay: 0.4 * index/4.8 }}
-                                        className="bg-slate-50 rounded-lg shadow-md">
+                                        className="bg-slate-50 rounded-lg shadow-md"
+                                        key={index}>
                                         <div className="w-full relative">
                                             <div className="absolute bg-green-400 text-black left-0 top-0 rounded-br-lg rounded-tl-lg shadow-sm p-2 font-mono text-sm">400 Kcal</div>
                                             <motion.div
@@ -130,13 +131,42 @@ export default function Dashboard({userDetail}: Record<string, any>) {
                                 );
                             })
                         }
-                        
-                    </div>
-                    
-                    
+                </div>
                 </motion.div>
+            </div>
+            
+            <div className="mt-5 mb-5 h-[5em]">
+            </div>
+
+            <div className="fixed bottom-0 left-0 w-full bg-[#FFA24B] text-white text-center p-4 z-50 h-[6.2em] md:h-[10em]">
+
+                <div className="flex justify-around md:justify-center md:gap-5 items-center">
+                
+                    <div className="grid auto-cols-max grid-flow-col gap-2">
+                        {
+                            Array.from({ length: 5 }).map((_, index) => (
+                            <div key={index} className="border-2 border-dashed border-slate-600 bg-orange-100 w-[2em] h-[2em] md:w-[6em] md:h-[6em] flex items-center justify-center">
+                                <FaPlus className="text-2xl text-slate-600" />
+                            </div>
+                            ))
+                        }
+                    </div>
+
+   
+                    <div className="px-4 md:px-0">
+                        <button disabled className="bg-gradient-to-r from-orange-500 via-red-400 to-orange-500 text-white font-extrabold text-sm sm:text-base md:text-lg uppercase tracking-wide px-5 py-3 sm:px-6 sm:py-3 md:px-8 md:py-4 rounded-full shadow-2xl hover:scale-105 hover:shadow-orange-500/50 transition-all duration-300 active:scale-95">
+                            Commander maintenant
+                        </button>
+                    </div>
+
+
+
+
+                </div>
+
 
             </div>
+
 
         </div>
     )
